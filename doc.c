980c33eaee6b3859aeeef7a006962143ca75159b
Hari Vardhan Reddy <93574351+golihari@users.noreply.github.com>
2022-04-23 19:10:32 +0530

    Create doc.c

---
+++ doc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int a, b, sum;
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b);
+   sum = a + b;
+   printf("Sum : %d", sum);
+   /* output=Enter two no: 5 6
+     Sum : 11 */
+   return(0);
+}
